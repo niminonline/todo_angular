aadaf2a377c30e2d1ae45b4c04d66ab0c0fe4ac5
niminonline <niminonline@gmail.com>
2023-08-15 05:16:06 +0530

    design-phase1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
 import { Component } from '@angular/core';
+import { Task } from './shared/task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
   title = 'todo';
+
+  now: Date = new Date();
+  period: number = this.now.getHours();
+  greeting!: string;
+  tasks: Task[]=[];
+  taskInput:string=''
+  constructor() {}
+
+  ngOnInit() {
+    this.getTimeData();
+  }
+  getTimeData() {
+    switch (true) {
+      case this.period >= 0 && this.period < 12:
+        this.greeting = 'Morning 🌤️';
+        break;
+      case this.period >= 12 && this.period < 16:
+        this.greeting = 'Afternoon ☀️';
+        break;
+      case this.period >= 16 && this.period < 19:
+        this.greeting = 'Evening 🌇';
+        break;
+      case this.period >= 19 && this.period < 24:
+        this.greeting = 'Evening 🌙';
+        break;
+
+      default:
+        this.greeting = 'Morning';
+    }
+  }
+
+  clearField(){
+  this.taskInput=""
+  }
+  addTask(){
+    this.tasks.push({
+      
+        id: Date.now(),
+        data: this.taskInput,
+        list: "OG",
+        date: new Date().toLocaleString("en-IN"),
+        priority: 0,
+    })
+    this.clearField();
+    console.log(this.tasks);
+
+  }
+
 }
